f27d3563cab62e81837e9f5479fc71e2aeb660ef
[Rafay123Anwar] <[muhammadrafayy0@gmail.com]>
2024-12-07 16:00:24 +0500

    initial

--- loginproject/settings.py
+++ loginproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'loginproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'loginproject/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
